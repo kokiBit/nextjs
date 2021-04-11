d6ed4ba64450a7efe0c4177001356609ad159d52
koki.takahashi <koki.takahashi1992@gmail.com>
2021-04-11 20:57:11 +0900

    メンテナンスモードの実験

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	redirects() {
+		return [
+			process.env.MAINTENANCE_MODE === '1'
+				? { source: '/((?!maintenance).*)', destination: '/maintenance.html', permanent: false }
+				: null,
+		].filter(Boolean);
+	}
+}
+  
